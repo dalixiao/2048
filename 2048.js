5cf5cfb581f586c596976c8397db7b67d4b64a16
dalixiao <daleyshaw96@gmail.com>
2020-05-07 16:11:25 +0800

    Initial two 2s set, now give color to the placeholder which holds number

--- 2048.js
+++ 2048.js
@@ -1,8 +1,17 @@
-CANVAS_SIZE = 600
+CANVAS_SIZE = 700
 CANVAS_BACKGROUND_COLOR = "#333333"
 GAME_SIZE = 4
 BLOCK_SIZE = 150
 BLOCK_PLACEHOLDER_COLOR = "red"
+
+// Global Utility Functions
+randInt = function(a,b){
+  return a + Math.floor(Math.random()*(b+1-a));
+}
+randChoice = function(arr){
+  return arr[randInt(0,arr.length-1)];
+}
+
 //Model
 class Game{
     constructor(){
@@ -13,12 +22,27 @@ class Game{
     initializeData(){
       this.data = [];
       for(let i = 0; i < GAME_SIZE ; i++){
-        temp = []
+        let temp = []
         for (let j = 0; j < GAME_SIZE; j++){
           temp.push(null);
         }
         this.data.push(temp)
       }
+      this.generateNewBlock()
+      this.generateNewBlock()
+    }
+
+    generateNewBlock(){
+      let possiblePositions = [];
+      for(let i = 0; i< GAME_SIZE;i++){
+        for(let j = 0; j < GAME_SIZE; j++){
+          if(this.data[i][j] == null){
+            possiblePositions.push([i,j]);
+          }
+        }
+      }
+      let position = randChoice(possiblePositions);
+      this.data[position[0]][position[1]] = 2;
     }
 
 }
@@ -51,12 +75,13 @@ class View{
      block.style.height = BLOCK_SIZE;
      block.style.backgroundColor = BLOCK_PLACEHOLDER_COLOR;
      block.style.position = "absolute"
-     block.style.top = i*BLOCK_SIZE;
-     block.style.left = j*BLOCK_SIZE;
+     block.style.top = (i+1)*20+i*BLOCK_SIZE;
+     block.style.left = (j+1)*  20+j*BLOCK_SIZE;
      this.container.append(block);
    }
 }
 //Controller
 var container =  document.getElementById("game-container");
+var game = new Game();
 var view = new View(container);
 view.drawGame();
